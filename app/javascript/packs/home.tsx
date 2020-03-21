0fe3889052e109a72683a19cfafca49d8fcaa1fb
ogihara-ryo <ogihara.ryo.b@gmail.com>
2020-03-22 07:39:07 +0900

    Home コンポーネントの TypeScript 対応

--- app/javascript/packs/home.tsx
+++ app/javascript/packs/home.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-const Home = props => (
+const Home = () => (
   <div>NEW GAME</div>
 )
 
